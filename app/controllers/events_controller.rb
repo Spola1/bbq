2f5c022637a8a4f27b87c8bbd696859b897ca579
Spola1 <1111.22224@mail.ru>
2022-05-10 10:49:08 +0300

    add avatar/access rights

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,51 +1,56 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index]
 
-  # GET /events or /events.json
+  # Задаем объект @event для экшена show
+  before_action :set_event, only: [:show]
+
+  # Задаем объект @event от текущего юзера для других действий
+  before_action :set_current_user_event, only: [:edit, :update, :destroy]
+
   def index
     @events = Event.all
   end
 
-  # GET /events/1 or /events/1.json
   def show
   end
 
-  # GET /events/new
   def new
-    @event = Event.new
+    @event = current_user.events.build
   end
 
-  # GET /events/1/edit
   def edit
   end
 
-  # POST /events or /events.json
   def create
-    @event = Event.new(event_params)
+    @event = current_user.events.build(event_params)
 
     if @event.save
-      redirect_to @event, notice: 'Event was successfully created.'
+      # Используем сообщение из файла локалей ru.yml
+      # controllers -> events -> created
+      redirect_to @event, notice: I18n.t('controllers.events.created')
     else
       render :new
     end
   end
 
-  # PATCH/PUT /events/1 or /events/1.json
   def update
     if @event.update(event_params)
-      redirect_to @event, notice: 'Event was successfully updated.'
+      redirect_to @event, notice: I18n.t('controllers.events.updated')
     else
       render :edit
     end
   end
 
-  # DELETE /events/1 or /events/1.json
   def destroy
     @event.destroy
-    redirect_to events_url, notice: 'Event was successfully destroyed.'
+    redirect_to events_url, notice: I18n.t('controllers.events.destroyed')
   end
 
   private
+
+  def set_current_user_event
+    @event = current_user.events.find(params[:id])
+  end
 
   def set_event
     @event = Event.find(params[:id])
