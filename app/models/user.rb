37f96a181596cafbf5c1531f52cfe0357f6a30e7
Spola1 <1111.22224@mail.ru>
2022-05-12 14:27:04 +0300

    delete user with all events

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
 
-  has_many :events
+  has_many :events, dependent: :delete_all
 
   validates :name, presence: true, length: {maximum: 35}
 
