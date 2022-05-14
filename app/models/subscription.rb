cf2c6d9fe84917d2cb2bd7017a99824c92123c75
Spola1 <1111.22224@mail.ru>
2022-05-14 17:18:47 +0300

    with_options + translate routes

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,14 +2,17 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
-  validates :event, presence: true
-  validates :user_name, presence: true, unless: -> { user.present? }
-  validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
-  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
-  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+  with_options unless: :user.present? do
+    validates :user_name, presence: true, unless: -> { user.present? }
+    validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
+    validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+    validate :current_email_already_use, unless: -> { user.present? && User.exists?(email: user_email) }
+  end
 
-  validate :current_email_already_use
-  validate :creator_cant_subscribe_self_event
+  with_options if: :user.present? do
+    validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
+    validate :creator_cant_subscribe_self_event, if: -> { event.user.email == user_email }
+  end
 
   def user_name
     if user.present?
@@ -28,14 +31,10 @@ class Subscription < ApplicationRecord
   end
 
   def current_email_already_use
-    if !user.present? && User.exists?(email: user_email)
-      errors.add(:user_email, I18n.t('errors.models.subscription.email_already_use'))
-    end
+    errors.add(:user_email, :email_already_use)
   end
 
   def creator_cant_subscribe_self_event
-    if event.user.email == user_email
-      errors.add(:user_email, I18n.t('errors.models.subscription.creator_cant_subscribe'))
-    end
+    errors.add(:user_email, :creator_cant_subscribe)
   end
 end
