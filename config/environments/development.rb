e62f778dc52933de19932867a6b60ddea6a907a8
Spola1 <1111.22224@mail.ru>
2022-05-19 18:18:02 +0300

    letter-opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,14 +70,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: '587',
-    user_name: ENV["MAILJET_SENDER"],
-    password: ENV["MAIL_PASSWORD"],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
